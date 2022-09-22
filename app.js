b0fd4418750d72d698745619bcf40695baa3fd15
Dharmelolar <hardeydoyeen1@gmail.com>
2022-09-22 13:51:19 +0100

    initial commit

--- app.js
+++ app.js
@@ -1,27 +1,28 @@
+const requestPermission = () => {
+  Notification.requestPermission().then((permission) => {
+    if (permission) {
+      const notify = new Notification("Mattermost Community", {
+        body: "There's a new chat in the community channel",
+        icon: "/mattermost.png",
+        vibrate:200
+      });
+
+      notify.addEventListener("click", () => {
+        window.open("https://mattermost.com/");
+      });
+      setTimeout(() => {
+        notify.close();
+      }, 10 * 1000);
+    } else {
+      null;
+    }
+  });
+};
+
+new Notification(title,{body,})
+
 if ("Notification" in window) {
-  console.log("available");
+  requestPermission();
 } else {
   console.log("not available");
 }
-
-const requestPermission = () => {
-  Notification.requestPermission().then((permission) =>
-    permission
-      ? console.log("permission granted")
-      : console.log("permissiondenied")
-  );
-  const notify = new Notification("Mattermost Community", {
-    body: "There's a new chat in the community channel",
-    icon: "/mattermost.png" ,
-  });
-
-  notify.addEventListener("click", ()=>{
-    window.open("https://mattermost.com/")
-  })
-};
-requestPermission();
-
-setTimeout(() => {
-  notify.close()
-}, 10 * 1000);
-
